6bfa0cc748a89cbc356226b9264366faf00c4171
yairch <50925578+yairch@users.noreply.github.com>
2021-06-14 20:47:53 +0300

    fixed GUI

--- GUI.py
+++ GUI.py
@@ -1,38 +1,48 @@
-from tkinter import Tk, Label, Button, Entry, END, W
+from tkinter import Tk, Label, Button, Entry, END, W, IntVar
 from tkinter import messagebox
 import tkinter as tk
 import os
+import naiveBayesModel
 
 
 class naiveBayesGUI:
 
     def __init__(self, master):
         self.path = ""
+        self.valid_path = False
+        self.valid_bins = False
         self.num_of_bins = None
+        self.feature_structure = None
+        self.model = None
 
+        # define window size
         canvas = tk.Canvas(master, width=300, height=100)
         canvas.grid(columnspan=3)
 
-        self.master = master
-        master.title("Naive Bayes Classifier")
+        self.master = master  # tkinter root
+        master.title("Naive Bayes Classifier")  # app title
 
+        # working directory path
         self.path_label = Label(master, text="Directory Path")
-
         register_path = master.register(self.set_path)  # we have to wrap the command
         self.path_entry = Entry(master)
         self.path_entry.config(validate="key", validatecommand=(register_path, '%P'))
-
         self.browse_button = Button(master, text="Browse", command=lambda: self.on_click_browse())
 
+        # number of bins input
         self.binning_label = Label(master, text="Discretization Bins")
-
-        self.test_classification_button = Button(master, text="Classify", command=lambda: self.on_click_classify())
-        self.train_model_button = Button(master, text="Build", command=lambda: self.on_click_build())
-
         register_binning = master.register(self.set_num_of_bins)  # we have to wrap the command
         self.binning_entry = Entry(master)
         self.binning_entry.config(validate="key", validatecommand=(register_binning, '%P'))
 
+        # model functionality
+        self.test_classification_button = Button(master, text="Classify", command=lambda: self.on_click_classify())
+        self.test_classification_button['state'] = 'disabled'
+
+        self.train_model_button = Button(master, text="Build", command=lambda: self.on_click_build())
+        self.train_model_button['state'] = 'disabled'
+
+        # design
         self.path_entry.grid(row=0, column=1, sticky=W)
         self.binning_entry.grid(row=1, column=1, sticky=W)
 
@@ -62,24 +72,35 @@ class naiveBayesGUI:
             messagebox.showerror(
                 title="Naive Bayes Classifier",
                 message="Error occurred when opening directory, no path specified.")
+            self.train_model_button['state'] = 'disabled'
         else:
             try:
 
                 files = os.listdir(self.path)
                 if train_filename in files and test_filename \
                         in files and structure_filename in files:
+                    self.valid_path = True
+                    messagebox.showinfo(
+                        title="Naive Bayes Classifier",
+                        message="Valid path has been set")
+
+                    if self.valid_path and self.valid_bins:
+                        self.train_model_button['state'] = 'normal'
+
                     return True
                 else:
                     messagebox.showerror(
                         title="Naive Bayes Classifier",
                         message="Error occurred when opening directory. Not all needed files present")
                     self.path_entry.delete(0, END)
+                    self.train_model_button['state'] = 'disabled'
                     return False
             except:
                 messagebox.showerror(
                     title="Naive Bayes Classifier",
                     message="Error occurred when opening directory. No directory named {0}".format(self.path))
                 self.path_entry.delete(0, END)
+                self.train_model_button['state'] = 'disabled'
                 return False
 
     def set_num_of_bins(self, bins):
@@ -87,44 +108,86 @@ class naiveBayesGUI:
         :param bins: number of bins as chosen by user
         :return: boolean value indicating validity on bins number
         """
-        if bins == "":  # no number of bins has been entereds
-            messagebox.showerror(
-                title="Naive Bayes Classifier",
-                message="No number of bins specified")
-            self.binning_entry.delete(0, END)
-            return False
+        if bins == "":  # no number of bins has been entered
+            self.num_of_bins = 0
+            self.train_model_button['state'] = 'disabled'
+            return True
         else:
             try:
                 bins = int(bins)
                 if bins > 0:
                     self.num_of_bins = bins
+                    self.valid_bins = True
+
+                    if self.valid_path and self.valid_bins:
+                        self.train_model_button['state'] = 'normal'
+
                     return True
                 else:
-                    messagebox.showerror(
-                        title="Naive Bayes Classifier",
-                        message="Invalid number of bins: {0}".format(bins))
-                    self.binning_entry.delete(0, END)
                     return False
             except ValueError:
-                self.binning_entry.delete(0, END)
-                messagebox.showerror(
-                    title="Naive Bayes Classifier",
-                    message="Invalid number of bins: {0}".format(bins))
                 return False
 
     def on_click_build(self):
 
-        if self.num_of_bins:
-            pass
-        # call function that loads the training set and trains the model
-        pass
+        if self.num_of_bins < 2:
+            messagebox.showerror(
+                title="Naive Bayes Classifier",
+                message="Bin amount must be at least 2")
+            return False
+
+        # load required files
+        train_set = naiveBayesModel.import_data(self.path, 'train.csv')
+        self.feature_structure = naiveBayesModel.read_structure(self.path, 'Structure.txt')
+
+        if train_set is False or not self.feature_structure:
+            messagebox.showerror(
+                title="Naive Bayes Classifier",
+                message="Either train set, test set, or structure file are empty")
+            return False
+
+        self.model = naiveBayesModel.build_model(train_set=train_set,
+                                                 feature_structure=self.feature_structure,
+                                                 n_bins=self.num_of_bins)
+        messagebox.showinfo(
+            title="Naive Bayes Classifier",
+            message="Building classifier using train-set is done!")
+
+        self.test_classification_button['state'] = 'normal'
+        return True
 
     def on_click_classify(self):
 
-        # call the function that loads the test file and classifies the entries
-        pass
+        # load test file
+        if not self.path or not self.feature_structure or not self.model:
+            messagebox.showerror(
+                title="Naive Bayes Classifier",
+                message="Model haven't been built")
+            return False
 
+        test_set = naiveBayesModel.import_data(self.path, 'test.csv')
+
+        if test_set is False:
+            messagebox.showerror(
+                title="Naive Bayes Classifier",
+                message="Test set is empty")
+            return False
+
+        predictions = naiveBayesModel.classify(model=self.model,
+                                               test_set=test_set,
+                                               feature_structure=self.feature_structure,
+                                               n_bins=self.num_of_bins)
+
+        naiveBayesModel.save_predictions(path=self.path,
+                                         predictions=predictions)
+        messagebox.showinfo(
+            title="Naive Bayes Classifier",
+            message="Classified successfuly and saved predictions. Press OK to exit.")
+        self.master.destroy()
+        return True
 
 root = Tk()
 my_gui = naiveBayesGUI(root)
 root.mainloop()
+
+# Todo: disable build and classify
